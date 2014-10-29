8301446354e98b6ad15e28868466bbe64180d0ec
summermarie98 <s631538@student.roundrockisd.org>
2014-10-29 15:23:04 -0500

    Lab 11 FINISHED!

--- lab11-100pt.py
+++ lab11-100pt.py
@@ -15,7 +15,7 @@ drawpad = Canvas(root, width=800,height=600, background='white')
 drawpad.grid(row=0, column=0)
 
 # Create the oval
-circle = drawpad.create_oval(10, 10, 50, 50, fill='green')
+circle = drawpad.create_oval(750, 10, 790, 50, fill='green')
 direction = 1
 # Create our animation function
 def animate():
@@ -23,7 +23,7 @@ def animate():
     # Get the x and y co-ordinates of the circle
     x1, y1, x2, y2 = drawpad.coords(circle)
     if x2 > drawpad.winfo_width(): 
-        direction = - 1
+        drawpad.move(circle,-800,0)
     elif x1 < 0:
         direction = 1
     #Move our oval object by the value of direction
--- lab11-70pt.py
+++ lab11-70pt.py
@@ -14,8 +14,8 @@ drawpad = Canvas(root, width=800,height=600, background='white')
 drawpad.grid(row=0, column=0)
 
 # Create the oval
-circle = drawpad.create_oval(10, 10, 50, 50, fill='green')
-direction = 1
+circle = drawpad.create_oval(10, 10, 100, 100, fill='green')
+direction = 5
 # Create our animation function
 def animate():
     global direction
@@ -26,7 +26,7 @@ def animate():
     elif x1 < 0:
         direction = 1
     #Move our oval object by the value of direction
-    drawpad.move(circle,direction,0)
+    drawpad.move(circle,5,0)
     # Wait for 1 millisecond, then recursively call our animate function
     drawpad.after(1, animate)
 
